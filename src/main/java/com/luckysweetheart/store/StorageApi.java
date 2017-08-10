f67ae2e99dc3a18af6fe365558e4ac66c7fd7dfe
yangxin <981987024@qq.com>
2017-08-10 17:06:15 +0800

    准备重构存储

--- src/main/java/com/luckysweetheart/store/StorageApi.java
+++ src/main/java/com/luckysweetheart/store/StorageApi.java
@@ -1,10 +1,12 @@
 package com.luckysweetheart.store;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by yangxin on 2017/6/12.
  */
+@Component
 public interface StorageApi {
-
 
 
 }
