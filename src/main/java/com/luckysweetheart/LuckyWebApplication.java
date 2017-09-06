6ec17dec54ccc05c92a90612d4e433cc74b79e96
yangxin <981987024@qq.com>
2017-09-06 09:56:48 +0800

    坑爹的打包方式

--- src/main/java/com/luckysweetheart/LuckyWebApplication.java
+++ src/main/java/com/luckysweetheart/LuckyWebApplication.java
@@ -1,10 +1,7 @@
 package com.luckysweetheart;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /*@SpringBootApplication
